2b5f30121de5388b9d99d33b4dafea6705e4cf6d
Quim Testar <quimtestar@gmail.com>
2014-04-20 12:53:21 +0200

    Just a little test.

--- Aletheia.Utilities/src/aletheia/utilities/Capsule.java
+++ Aletheia.Utilities/src/aletheia/utilities/Capsule.java
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with the Aletheia Proof Assistant. If not, see
  * <http://www.gnu.org/licenses/>.
+ * 
+ * prova
  ******************************************************************************/
 package aletheia.utilities;
 
