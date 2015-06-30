804396ef9f3fb852def86b6f2c12ed8e77395b32
Quim Testar <quimtestar@gmail.com>
2015-06-30 07:12:04 +0200

    Aletheia.Version project. For somehow obtaininig the "git describe" result when built at runtime.

---
+++ Aletheia.Version/src/aletheia/version/VersionManager.java
@@ -0,0 +1,44 @@
+package aletheia.version;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class VersionManager
+{
+	private final static VersionManager instance;
+	static
+	{
+		try
+		{
+			instance = new VersionManager();
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	private final String version;
+
+	private VersionManager() throws IOException
+	{
+		InputStream is = ClassLoader.getSystemResourceAsStream("aletheia/version/version.txt");
+		try
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			version = br.readLine();
+		}
+		finally
+		{
+			is.close();
+		}
+	}
+
+	public static String getVersion()
+	{
+		return instance.version;
+	}
+
+}
