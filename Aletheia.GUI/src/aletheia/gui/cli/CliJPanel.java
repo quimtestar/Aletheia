fcac7589bb5028cd0397f74397d80e8339c2e557
Quim Testar <quimtestar@gmail.com>
2015-10-21 19:07:59 +0200

    Fix: avoiding exception when reaching out of limits of document.

--- Aletheia.GUI/src/aletheia/gui/cli/CliJPanel.java
+++ Aletheia.GUI/src/aletheia/gui/cli/CliJPanel.java
@@ -442,9 +442,8 @@ public class CliJPanel extends JPanel
 		{
 			try
 			{
-				if (length > 0)
+				if ((length > 0) && (offset -multiLinePrompt.length()-1>=0))
 				{
-					//TODO out limits!
 					String s = document.getText(offset - multiLinePrompt.length() - 1, multiLinePrompt.length() + 2);
 					int i = s.indexOf('\n');
 					if (i >= 0)
