b03ece58d824cac4ccb99f872678f29dcf35a8b3
Quim Testar <quimtestar@gmail.com>
2016-06-17 19:28:02 +0200

    Moved the 'NamespacePattern' class to its own file in the common package.

--- Aletheia.GUI/src/aletheia/gui/cli/command/statement/Search.java
+++ Aletheia.GUI/src/aletheia/gui/cli/command/statement/Search.java
@@ -22,17 +22,13 @@ package aletheia.gui.cli.command.statement;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.regex.Pattern;
 
 import aletheia.gui.cli.command.CommandSource;
 import aletheia.gui.cli.command.AbstractVoidCommandFactory;
 import aletheia.gui.cli.command.TaggedCommand;
 import aletheia.gui.cli.command.TransactionalCommand;
+import aletheia.gui.common.NamespacePattern;
 import aletheia.model.identifier.Identifier;
-import aletheia.model.identifier.Namespace;
-import aletheia.model.identifier.NodeNamespace;
-import aletheia.model.identifier.NodeNamespace.InvalidNameException;
-import aletheia.model.identifier.RootNamespace;
 import aletheia.model.statement.Context;
 import aletheia.model.statement.Statement;
 import aletheia.persistence.Transaction;
@@ -41,167 +37,6 @@ import aletheia.utilities.collections.CloseableIterator;
 @TaggedCommand(tag = "search", groupPath = "/statement", factory = Search.Factory.class)
 public class Search extends TransactionalCommand
 {
-	private static abstract class NamespacePattern
-	{
-
-		public static NamespacePattern instantiate(String expression)
-		{
-			try
-			{
-				return new ExpressionNamespacePattern(expression);
-			}
-			catch (InvalidNameException e)
-			{
-				return new VoidNamespacePattern();
-			}
-		}
-
-		public abstract Identifier fromKey();
-
-		public abstract boolean isPrefix(Identifier identifier);
-
-		public abstract boolean matches(Namespace namespace);
-
-		private static class VoidNamespacePattern extends NamespacePattern
-		{
-
-			private VoidNamespacePattern()
-			{
-
-			}
-
-			@Override
-			public Identifier fromKey()
-			{
-				return RootNamespace.instance.terminator();
-			}
-
-			@Override
-			public boolean isPrefix(Identifier identifier)
-			{
-				return false;
-			}
-
-			@Override
-			public boolean matches(Namespace namespace)
-			{
-				return false;
-			}
-
-		}
-
-		private static class ExpressionNamespacePattern extends NamespacePattern
-		{
-			private final Namespace prefix;
-			private final boolean dottedPrefix;
-			private final Pattern pattern;
-
-			private ExpressionNamespacePattern(String expression) throws InvalidNameException
-			{
-				int p = expression.length();
-
-				int iq = expression.indexOf('?');
-				if ((iq >= 0) && (iq < p))
-					p = iq;
-
-				int ia = expression.indexOf('*');
-				if ((ia >= 0) && (ia < p))
-					p = ia;
-
-				int ih = expression.indexOf('#');
-				if ((ih >= 0) && (ih < p))
-					p = ih;
-				String sPrefix = expression.substring(0, p);
-				this.prefix = Namespace.parse(sPrefix);
-				this.dottedPrefix = sPrefix.endsWith(".");
-
-				StringBuilder regex = new StringBuilder();
-				int i = 0;
-				for (String s : expression.split("[\\*\\?#]"))
-				{
-					regex.append(Pattern.quote(s));
-					i += s.length();
-					loop: while (i < expression.length())
-					{
-						switch (expression.charAt(i))
-						{
-						case '?':
-							regex.append(".");
-							break;
-						case '*':
-							regex.append(".*");
-							break;
-						case '#':
-							regex.append("[0-9]");
-							break;
-						default:
-							break loop;
-						}
-						i++;
-					}
-				}
-				loop: while (i < expression.length())
-				{
-					switch (expression.charAt(i))
-					{
-					case '?':
-						regex.append(".");
-						break;
-					case '*':
-						regex.append(".*");
-						break;
-					case '#':
-						regex.append("[0-9]");
-						break;
-					default:
-						break loop;
-					}
-					i++;
-				}
-				this.pattern = Pattern.compile(regex.toString());
-			}
-
-			@Override
-			public Identifier fromKey()
-			{
-				if (prefix instanceof NodeNamespace)
-					return ((NodeNamespace) prefix).asIdentifier();
-				else if (prefix instanceof RootNamespace)
-					return ((RootNamespace) prefix).initiator();
-				else
-					throw new Error();
-			}
-
-			@Override
-			public boolean isPrefix(Identifier identifier)
-			{
-				if (dottedPrefix || prefix instanceof RootNamespace)
-					return prefix.isPrefixOf(identifier);
-				else if (prefix instanceof NodeNamespace)
-				{
-					NodeNamespace nPrefix = (NodeNamespace) prefix;
-					if (!nPrefix.getParent().isPrefixOf(identifier))
-						return false;
-					Namespace suffix = identifier.makeSuffix(nPrefix.getParent());
-					if (suffix instanceof NodeNamespace)
-						if (!((NodeNamespace) suffix).headName().startsWith(nPrefix.getName()))
-							return false;
-					return true;
-				}
-				else
-					throw new Error();
-			}
-
-			@Override
-			public boolean matches(Namespace namespace)
-			{
-				return pattern.matcher(namespace.qualifiedName()).matches();
-			}
-
-		}
-
-	}
-
 	private final NamespacePattern namespacePattern;
 
 	public Search(CommandSource from, Transaction transaction, String expression)
@@ -263,7 +98,7 @@ public class Search extends TransactionalCommand
 		@Override
 		public String shortHelp()
 		{
-			return "Search for a particular statement's identifier pattern in the active context (with wildcards '?' and '*').";
+			return "Search for a particular statement's identifier pattern in the active context (with wildcards '?' (one char), '#' (one digit) and '*' (any string)).";
 		}
 
 	}
---
+++ Aletheia.GUI/src/aletheia/gui/common/NamespacePattern.java
@@ -0,0 +1,189 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Quim Testar.
+ * 
+ * This file is part of the Aletheia Proof Assistant.
+ * 
+ * The Aletheia Proof Assistant is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * The Aletheia Proof Assistant is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Aletheia Proof Assistant. If not, see
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package aletheia.gui.common;
+
+import java.util.regex.Pattern;
+
+import aletheia.model.identifier.Identifier;
+import aletheia.model.identifier.Namespace;
+import aletheia.model.identifier.NodeNamespace;
+import aletheia.model.identifier.RootNamespace;
+import aletheia.model.identifier.NodeNamespace.InvalidNameException;
+
+public abstract class NamespacePattern
+{
+
+	public static NamespacePattern instantiate(String expression)
+	{
+		try
+		{
+			return new ExpressionNamespacePattern(expression);
+		}
+		catch (InvalidNameException e)
+		{
+			return new VoidNamespacePattern();
+		}
+	}
+
+	public abstract Identifier fromKey();
+
+	public abstract boolean isPrefix(Identifier identifier);
+
+	public abstract boolean matches(Namespace namespace);
+
+	private static class VoidNamespacePattern extends NamespacePattern
+	{
+
+		private VoidNamespacePattern()
+		{
+
+		}
+
+		@Override
+		public Identifier fromKey()
+		{
+			return RootNamespace.instance.terminator();
+		}
+
+		@Override
+		public boolean isPrefix(Identifier identifier)
+		{
+			return false;
+		}
+
+		@Override
+		public boolean matches(Namespace namespace)
+		{
+			return false;
+		}
+
+	}
+
+	private static class ExpressionNamespacePattern extends NamespacePattern
+	{
+		private final Namespace prefix;
+		private final boolean dottedPrefix;
+		private final Pattern pattern;
+
+		private ExpressionNamespacePattern(String expression) throws InvalidNameException
+		{
+			int p = expression.length();
+
+			int iq = expression.indexOf('?');
+			if ((iq >= 0) && (iq < p))
+				p = iq;
+
+			int ia = expression.indexOf('*');
+			if ((ia >= 0) && (ia < p))
+				p = ia;
+
+			int ih = expression.indexOf('#');
+			if ((ih >= 0) && (ih < p))
+				p = ih;
+			String sPrefix = expression.substring(0, p);
+			this.prefix = Namespace.parse(sPrefix);
+			this.dottedPrefix = sPrefix.endsWith(".");
+
+			StringBuilder regex = new StringBuilder();
+			int i = 0;
+			for (String s : expression.split("[\\*\\?#]"))
+			{
+				regex.append(Pattern.quote(s));
+				i += s.length();
+				loop: while (i < expression.length())
+				{
+					switch (expression.charAt(i))
+					{
+					case '?':
+						regex.append(".");
+						break;
+					case '*':
+						regex.append(".*");
+						break;
+					case '#':
+						regex.append("[0-9]");
+						break;
+					default:
+						break loop;
+					}
+					i++;
+				}
+			}
+			loop: while (i < expression.length())
+			{
+				switch (expression.charAt(i))
+				{
+				case '?':
+					regex.append(".");
+					break;
+				case '*':
+					regex.append(".*");
+					break;
+				case '#':
+					regex.append("[0-9]");
+					break;
+				default:
+					break loop;
+				}
+				i++;
+			}
+			this.pattern = Pattern.compile(regex.toString());
+		}
+
+		@Override
+		public Identifier fromKey()
+		{
+			if (prefix instanceof NodeNamespace)
+				return ((NodeNamespace) prefix).asIdentifier();
+			else if (prefix instanceof RootNamespace)
+				return ((RootNamespace) prefix).initiator();
+			else
+				throw new Error();
+		}
+
+		@Override
+		public boolean isPrefix(Identifier identifier)
+		{
+			if (dottedPrefix || prefix instanceof RootNamespace)
+				return prefix.isPrefixOf(identifier);
+			else if (prefix instanceof NodeNamespace)
+			{
+				NodeNamespace nPrefix = (NodeNamespace) prefix;
+				if (!nPrefix.getParent().isPrefixOf(identifier))
+					return false;
+				Namespace suffix = identifier.makeSuffix(nPrefix.getParent());
+				if (suffix instanceof NodeNamespace)
+					if (!((NodeNamespace) suffix).headName().startsWith(nPrefix.getName()))
+						return false;
+				return true;
+			}
+			else
+				throw new Error();
+		}
+
+		@Override
+		public boolean matches(Namespace namespace)
+		{
+			return pattern.matcher(namespace.qualifiedName()).matches();
+		}
+
+	}
+
+}
