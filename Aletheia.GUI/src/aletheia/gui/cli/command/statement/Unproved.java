f052989444831945afe81f789961d5c2aa504701
Quim Testar <quimtestar@gmail.com>
2016-11-17 16:58:32 +0100

    "Unproved" command to list unproved statements.

---
+++ Aletheia.GUI/src/aletheia/gui/cli/command/statement/Unproved.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Quim Testar.
+ *
+ * This file is part of the Aletheia Proof Assistant.
+ *
+ * The Aletheia Proof Assistant is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * The Aletheia Proof Assistant is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Aletheia Proof Assistant. If not, see
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package aletheia.gui.cli.command.statement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import aletheia.gui.cli.command.CommandSource;
+import aletheia.gui.cli.command.AbstractVoidCommandFactory;
+import aletheia.gui.cli.command.TaggedCommand;
+import aletheia.gui.cli.command.TransactionalCommand;
+import aletheia.model.statement.Context;
+import aletheia.model.statement.Statement;
+import aletheia.persistence.Transaction;
+import aletheia.utilities.collections.ReverseList;
+
+@TaggedCommand(tag = "unproved", groupPath = "/authority", factory = Unproved.Factory.class)
+public class Unproved extends TransactionalCommand
+{
+	private final Context context;
+
+	public Unproved(CommandSource from, Transaction transaction, Context context)
+	{
+		super(from, transaction);
+		this.context = context;
+	}
+
+	@Override
+	protected RunTransactionalReturnData runTransactional() throws Exception
+	{
+		Stack<Context> stack = new Stack<>();
+		stack.push(context);
+		while (!stack.isEmpty())
+		{
+			Context ctx = stack.pop();
+			ArrayList<Context> list = new ArrayList<>();
+			for (Statement st : ctx.localSortedStatements(getTransaction()))
+			{
+				if (!st.isProved())
+				{
+					getOut().println(" -> " + st.statementPathString(getTransaction(), getActiveContext()) + " " + (st.isProved() ? "\u2713" : ""));
+					if (st instanceof Context)
+						list.add((Context) st);
+					stack.addAll(new ReverseList<>(list));
+				}
+			}
+		}
+		return null;
+	}
+
+	public static class Factory extends AbstractVoidCommandFactory<Unproved>
+	{
+
+		@Override
+		protected int minParameters()
+		{
+			return 0;
+		}
+
+		@Override
+		public Unproved parse(CommandSource from, Transaction transaction, Void extra, List<String> split) throws CommandParseException
+		{
+			try
+			{
+				checkMinParameters(split);
+				Context context;
+				if (split.size() > 0)
+				{
+					Statement statement = findStatementSpec(from.getPersistenceManager(), transaction, from.getActiveContext(), split.get(0));
+					if (statement == null)
+						throw new CommandParseException("Invalid statement");
+					if (!(statement instanceof Context))
+						throw new CommandParseException("Not a context");
+					context = (Context) statement;
+				}
+				else
+				{
+					context = from.getActiveContext();
+					if (context == null)
+						throw new NotActiveContextException();
+				}
+				return new Unproved(from, transaction, context);
+			}
+			catch (NotActiveContextException e)
+			{
+				throw new CommandParseException(e);
+			}
+		}
+
+		@Override
+		protected String paramSpec()
+		{
+			return "[<context>]";
+		}
+
+		@Override
+		public String shortHelp()
+		{
+			return "Lists all the unproved statements descending of the specified (or the active) context.";
+		}
+
+	}
+
+}
