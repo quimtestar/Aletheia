4e58707c85e0dc798c96287b46c1e48bc0bf1254
Quim Testar <quimtestar@gmail.com>
2017-03-26 20:39:11 +0200

    Refinement of the '?' (term match) operator.

--- Aletheia.Base/src/aletheia/parser/tokenprocessor/C_C_question_A_TermTokenSubProcessor.java
+++ Aletheia.Base/src/aletheia/parser/tokenprocessor/C_C_question_A_TermTokenSubProcessor.java
@@ -1,13 +1,14 @@
 package aletheia.parser.tokenprocessor;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-
 import aletheia.model.identifier.Identifier;
 import aletheia.model.statement.Context;
-import aletheia.model.term.FunctionTerm;
 import aletheia.model.term.ParameterVariableTerm;
 import aletheia.model.term.Term;
+import aletheia.model.term.VariableTerm;
 import aletheia.parser.TermParserException;
 import aletheia.parser.tokenprocessor.parameterRef.ParameterRef;
 import aletheia.parsergenerator.tokens.NonTerminalToken;
@@ -29,14 +30,13 @@ public class C_C_question_A_TermTokenSubProcessor extends TermTokenSubProcessor
 	{
 		Term term = getProcessor().processTerm((NonTerminalToken) token.getChildren().get(0), input, context, transaction, tempParameterTable);
 		Term termMatch = getProcessor().processTerm((NonTerminalToken) token.getChildren().get(2), input, context, transaction, tempParameterTable);
-		if (!(termMatch instanceof FunctionTerm))
-			throw new TermParserException("Not a function term.", token.getChildren().get(2).getStartLocation(), token.getChildren().get(2).getStopLocation(),
-					input);
-		FunctionTerm functionMatch = (FunctionTerm) termMatch;
-		Term.Match match = functionMatch.getBody().match(Collections.singleton(functionMatch.getParameter()), term);
+		List<VariableTerm> assignable = new ArrayList<>();
+		Term.Match match = termMatch.consequent(assignable).match(new HashSet<>(assignable), term);
 		if (match == null)
 			throw new TermParserException("No match.", token.getStartLocation(), token.getStopLocation(), input);
-		return match.getAssignMapLeft().get(functionMatch.getParameter());
+		if (assignable.isEmpty())
+			throw new TermParserException("Nothing assignable.", token.getStartLocation(), token.getStopLocation(), input);
+		return match.getAssignMapLeft().get(assignable.get(0));
 	}
 
 }
