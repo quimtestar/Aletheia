7f554655527361b9f1a1e7db6e83c2e3103e14ee
Quim Testar <quimtestar@gmail.com>
2017-03-31 22:33:49 +0200

    Copyright headers.

--- Aletheia.Base/src/aletheia/parser/tokenprocessor/Sts_uuid_StatementReferenceTokenSubProcessor.java
+++ Aletheia.Base/src/aletheia/parser/tokenprocessor/Sts_uuid_StatementReferenceTokenSubProcessor.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Quim Testar.
+ * 
+ * This file is part of the Aletheia Proof Assistant.
+ * 
+ * The Aletheia Proof Assistant is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * The Aletheia Proof Assistant is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Aletheia Proof Assistant. If not, see
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package aletheia.parser.tokenprocessor;
 
 import java.util.UUID;
