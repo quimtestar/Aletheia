ff56b9ff6153459e2761b6cba245555fd4c32216
Quim Testar <quimtestar@gmail.com>
2017-08-13 11:31:04 +0200

    The termToString with list of assumptions (for the stout command) now uses the new ParameterIdentification solution.

--- Aletheia.GUI/src/aletheia/gui/cli/command/Command.java
+++ Aletheia.GUI/src/aletheia/gui/cli/command/Command.java
@@ -24,11 +24,8 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-
+import java.util.Stack;
 import aletheia.gui.app.AletheiaJFrame;
 import aletheia.gui.cli.command.CommandSource;
 import aletheia.model.authority.UnpackedSignatureRequest;
@@ -39,16 +36,12 @@ import aletheia.model.statement.Assumption;
 import aletheia.model.statement.Context;
 import aletheia.model.statement.Statement;
 import aletheia.model.term.FunctionTerm;
-import aletheia.model.term.ParameterVariableTerm;
 import aletheia.model.term.Term;
-import aletheia.model.term.VariableTerm;
 import aletheia.parser.AletheiaParserException;
 import aletheia.peertopeer.PeerToPeerNode;
 import aletheia.persistence.PersistenceManager;
 import aletheia.persistence.Transaction;
 import aletheia.prooffinder.ProofFinder;
-import aletheia.utilities.collections.AdaptedMap;
-import aletheia.utilities.collections.CombinedMap;
 
 public abstract class Command
 {
@@ -316,26 +309,13 @@ public abstract class Command
 
 	protected static String termToString(Context ctx, Transaction transaction, Term term, List<Assumption> assumptions)
 	{
-		Map<ParameterVariableTerm, Identifier> localVariableToIdentifier = new HashMap<>();
-		{
-			Term body = term;
-			Iterator<Assumption> assumptionIterator = assumptions.iterator();
-			while (body instanceof FunctionTerm)
-			{
-				FunctionTerm function = (FunctionTerm) body;
-				Assumption assumption = null;
-				if (assumptionIterator.hasNext())
-					assumption = assumptionIterator.next();
-				if (function.getBody().freeVariables().contains(function.getParameter()) && assumption != null && assumption.getIdentifier() != null)
-					localVariableToIdentifier.put(function.getParameter(), assumption.getIdentifier());
-				body = function.getBody();
-			}
-		}
-		if (ctx == null)
-			return term.toString(localVariableToIdentifier);
-		else
-			return term.toString(new CombinedMap<>(new AdaptedMap<VariableTerm, Identifier>(localVariableToIdentifier),
-					new AdaptedMap<VariableTerm, Identifier>(ctx.variableToIdentifier(transaction))));
+		Stack<Identifier> stack = new Stack<>();
+		for (Assumption a : assumptions)
+			stack.push(a.getIdentifier());
+		Term.ParameterIdentification parameterIdentification = null;
+		while (!stack.isEmpty())
+			parameterIdentification = new FunctionTerm.FunctionParameterIdentification(stack.pop(), null, parameterIdentification);
+		return term.toString(transaction, ctx, parameterIdentification);
 	}
 
 	public static Command parse(CommandSource from, Transaction transaction, String command) throws CommandParseException
