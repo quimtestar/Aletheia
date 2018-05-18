ff6a53075e4068ee6600bd63862231d9b42d3ac6
Quim Testar <quimtestar@gmail.com>
2018-05-18 20:22:25 +0200

    Test0000

---
+++ Aletheia.Testing/src/aletheia/test/Test.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Quim Testar
+ * 
+ * This file is part of the Aletheia Proof Assistant.
+ * 
+ * The Aletheia Proof Assistant is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * The Aletheia Proof Assistant is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Aletheia Proof Assistant. If not, see
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package aletheia.test;
+
+import java.io.File;
+
+import aletheia.test.unsorted.Test0000;
+
+public abstract class Test
+{
+
+	public abstract void run() throws Exception;
+
+	public static void main(String[] args) throws Exception
+	{
+		File dbFile = new File("/mnt/vlb0/quim/Aletheia/aletheiadb");
+		Test test = new Test0000(dbFile);
+		test.run();
+	}
+
+}
