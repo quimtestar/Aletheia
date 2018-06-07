0915e5b08ee4d62aaf21f0886f9d5b391e9ed38b
Quim Testar <quim.testar@awaait.com>
2018-06-07 19:04:58 +0200

    Problem detected when computing the proofTerm of the theorem Integer.gcd.th.product (UUID: 6f7ec943-8027-5ed0-b727-ebea5ac7cb0d)

--- Aletheia.Testing/src/aletheia/test/unsorted/parse_on_the_fly/Test0000.java
+++ Aletheia.Testing/src/aletheia/test/unsorted/parse_on_the_fly/Test0000.java
@@ -40,13 +40,14 @@ public class Test0000 extends TransactionalBerkeleyDBPersistenceManagerTest
 	@Override
 	protected void run(BerkeleyDBPersistenceManager persistenceManager, Transaction transaction) throws FileNotFoundException
 	{
-		UUID uuid = UUID.fromString("8bf0bf35-c253-5257-b3ef-49b59beabc77"); // Set.th.epsilon_induction
+		//UUID uuid = UUID.fromString("8bf0bf35-c253-5257-b3ef-49b59beabc77"); 		// Set.th.epsilon_induction
 		//UUID uuid=UUID.fromString("e7a380b5-3b72-5341-b868-56226e163779");		// Prop.And.th.symm/s001
 		//UUID uuid=UUID.fromString("360e41c6-2713-50ba-a22f-1f9d69dd1143");		// Set
 		//UUID uuid = UUID.fromString("db8546ce-ab3b-5441-98ca-b2fed77e695d"); 		// Set.Equal.def
 		//UUID uuid = UUID.fromString("9209090d-fc0c-5e24-b1e4-1d509857a328"); 		// Natural.two.th.sum.one
 		//UUID uuid = UUID.fromString("ac179280-c294-5812-b653-8bdc1dd9f24e"); 		// Natural.th.fundamental.arithmetic
 		//UUID uuid = UUID.fromString("13401716-ea4c-542e-9951-e69241578080"); 		// Real.e.th.summation.factorial
+		UUID uuid = UUID.fromString("6f7ec943-8027-5ed0-b727-ebea5ac7cb0d"); // Integer.gcd.th.product
 
 		Statement statement = persistenceManager.getStatement(transaction, uuid);
 		Term term = statement.proofTerm(transaction);
