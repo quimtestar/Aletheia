1718a38768dc749e3f09316146746b4ea34ab366
Quim Testar <quim.testar@awaait.com>
2019-07-18 16:46:59 +0200

    Just one test every time.

--- Aletheia.Testing/src/aletheia/test/Test.java
+++ Aletheia.Testing/src/aletheia/test/Test.java
@@ -23,6 +23,8 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import aletheia.utilities.CommandLineArguments;
 import aletheia.utilities.CommandLineArguments.CommandLineArgumentsException;
@@ -43,26 +45,24 @@ public abstract class Test
 		try
 		{
 			CommandLineArguments cla = new CommandLineArguments(args);
-			for (Parameter parameter : cla.getParameters())
+			String className = Optional.ofNullable(MiscUtilities.firstFromIterable(cla.getParameters())).map((Parameter p) -> p.getValue()).get();
+			try
 			{
-				String className = parameter.getValue();
-				try
+				@SuppressWarnings("unchecked")
+				Class<? extends Test> testClass = (Class<? extends Test>) ClassLoader.getSystemClassLoader().loadClass(className);
+				if (!Test.class.isAssignableFrom(testClass))
 				{
-					@SuppressWarnings("unchecked")
-					Class<? extends Test> testClass = (Class<? extends Test>) ClassLoader.getSystemClassLoader().loadClass(className);
-					if (!Test.class.isAssignableFrom(testClass))
-					{
-						System.err.format("Class '%s' does not derive from '%s'\n", className, Test.class.getName());
-						continue;
-					}
-					if (Modifier.isAbstract(testClass.getModifiers()))
-					{
-						System.err.format("Class '%s' is abstract\n", className);
-						continue;
-					}
+					System.err.format("Class '%s' does not derive from '%s'\n", className, Test.class.getName());
+				}
+				else if (Modifier.isAbstract(testClass.getModifiers()))
+				{
+					System.err.format("Class '%s' is abstract\n", className);
+				}
+				else
+				{
 					if (cla.getGlobalSwitches().containsKey("i"))
 					{
-						System.out.format("Run test class '%s'?", className);
+						System.out.format("Run test class '%s'?\n", className);
 						try
 						{
 							System.in.read();
@@ -75,53 +75,57 @@ public abstract class Test
 					Constructor<? extends Test> constructor = testClass.getConstructor();
 					Test test = constructor.newInstance();
 					test.run();
-				}
-				catch (ClassNotFoundException e)
-				{
-					System.err.format("Class '%s' not found\n", className);
-				}
-				catch (NoSuchMethodException e)
-				{
-					System.err.format("Class '%s' have not a no-argument constructor\n", className);
-				}
-				catch (SecurityException e)
-				{
-					System.err.format("No-argument constructor for class '%s' is not accessible\n", className);
-					e.printStackTrace();
-				}
-				catch (InstantiationException e)
-				{
-					System.err.format("Class '%s' could not be instantiated\n", className);
-					e.printStackTrace();
-				}
-				catch (IllegalAccessException e)
-				{
-					System.err.format("Illegal access instantiating class '%s'\n", className);
-					e.printStackTrace();
-				}
-				catch (IllegalArgumentException e)
-				{
-					System.err.format("Illegal argument instantiating class '%s'\n", className);
-					e.printStackTrace();
-				}
-				catch (InvocationTargetException e)
-				{
-					System.err.format("Exception '%s' thrown when instantiating class '%s'\n", e.getCause().getClass().getName(), className);
-					e.printStackTrace();
-				}
-				catch (Exception e)
-				{
-					System.err.format("Exception '%s' thrown when running test instance of '%s'\n", e.getCause().getClass().getName(), className);
-					e.printStackTrace();
+					System.exit(0);
 				}
 			}
-			System.exit(0);
+			catch (ClassNotFoundException e)
+			{
+				System.err.format("Class '%s' not found\n", className);
+			}
+			catch (NoSuchMethodException e)
+			{
+				System.err.format("Class '%s' have not a public no-argument constructor\n", className);
+			}
+			catch (SecurityException e)
+			{
+				System.err.format("Access to class '%s' is denied\n", className);
+				e.printStackTrace();
+			}
+			catch (InstantiationException e)
+			{
+				System.err.format("Class '%s' could not be instantiated\n", className);
+				e.printStackTrace();
+			}
+			catch (IllegalAccessException e)
+			{
+				System.err.format("Illegal access instantiating class '%s'\n", className);
+				e.printStackTrace();
+			}
+			catch (IllegalArgumentException e)
+			{
+				System.err.format("Illegal argument instantiating class '%s'\n", className);
+				e.printStackTrace();
+			}
+			catch (InvocationTargetException e)
+			{
+				System.err.format("Exception '%s' thrown when instantiating class '%s'\n", e.getCause().getClass().getName(), className);
+				e.printStackTrace();
+			}
+			catch (Exception e)
+			{
+				System.err.format("Exception '%s' thrown when running test instance of '%s'\n", e.getClass().getName(), className);
+				e.printStackTrace();
+			}
 		}
 		catch (CommandLineArgumentsException e)
 		{
 			System.err.println(e.getMessage());
-			System.exit(1);
 		}
+		catch (NoSuchElementException e)
+		{
+			System.err.format("Usage: java %s [-i] <test class>", Test.class.getName());
+		}
+		System.exit(1);
 	}
 
 }
