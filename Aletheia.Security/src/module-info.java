2dc64b042d2f3875989d2320d5cc8b6d64f82175
Quim Testar <quimtestar@gmail.com>
2020-04-10 23:06:06 +0200

    Module aletheia.security configured.

---
+++ Aletheia.Security/src/module-info.java
@@ -0,0 +1,11 @@
+module aletheia.security
+{
+	exports aletheia.security.messagedigester;
+	exports aletheia.security.utilities;
+	exports aletheia.protocol.security;
+	exports aletheia.model.security;
+	exports aletheia.security.signerverifier;
+
+	requires aletheia.protocol;
+	requires aletheia.utilities;
+}
