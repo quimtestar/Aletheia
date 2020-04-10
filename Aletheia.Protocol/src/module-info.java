53569367bf3e2b9649a2b9a39d0acdb8d1647180
Quim Testar <quimtestar@gmail.com>
2020-04-10 23:04:53 +0200

    Module aletheia.protocol configured.

---
+++ Aletheia.Protocol/src/module-info.java
@@ -0,0 +1,10 @@
+module aletheia.protocol
+{
+	exports aletheia.protocol.collection;
+	exports aletheia.protocol.enumerate;
+	exports aletheia.protocol;
+	exports aletheia.protocol.net;
+	exports aletheia.protocol.primitive;
+
+	requires aletheia.utilities;
+}
