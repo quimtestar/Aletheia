43b3b3e7d994dad5b6bc81edb4e795655677dbea
Quim Testar <quimtestar@gmail.com>
2020-04-12 13:28:09 +0200

    Reconfigured module-info.java of project Aletheia.PdfExport and dependencies.

--- Aletheia.PdfExport/src/module-info.java
+++ Aletheia.PdfExport/src/module-info.java
@@ -1,12 +1,5 @@
 module aletheia.pdfexport
 {
-	exports aletheia.pdfexport.statement;
-	exports aletheia.pdfexport;
-	exports aletheia.pdfexport.document;
-	exports aletheia.pdfexport.font;
-	exports aletheia.pdfexport.term;
-
-	requires transitive aletheia.base;
-	requires aletheia.utilities;
-	requires transitive itextpdf;
+	requires aletheia.base;
+	requires itextpdf;
 }
