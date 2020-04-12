6403e45ab0323939703ec7a85e1ffffb7a3eef69
Quim Testar <quimtestar@gmail.com>
2020-04-12 13:25:32 +0200

    Reconfigured module-info.java of project Aletheia.ProofFinder and dependencies.

--- Aletheia.ProofFinder/src/module-info.java
+++ Aletheia.ProofFinder/src/module-info.java
@@ -1,10 +1,6 @@
 module aletheia.prooffinder
 {
-	exports aletheia.prooffinder;
-
-	requires transitive aletheia.base;
+	requires aletheia.base;
 	requires aletheia.log4j;
-	requires aletheia.utilities;
 	requires java.management;
-	requires org.apache.logging.log4j;
 }
