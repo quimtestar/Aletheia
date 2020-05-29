94d70cf8b4aaabd1fcf3a88f1a13d2b6f04a1634
Quim Testar <quimtestar@gmail.com>
2020-05-29 21:44:41 +0200

    Opening needed packages to module je.

--- Aletheia.BerkeleyDB/src/module-info.java
+++ Aletheia.BerkeleyDB/src/module-info.java
@@ -22,4 +22,20 @@ module aletheia.persistence.berkeleydb
 	requires jdk.unsupported;
 	requires java.transaction.xa;
 	requires java.desktop;
+	
+	opens aletheia.persistence.berkeleydb.entities to je;
+	opens aletheia.persistence.berkeleydb.entities.authority to je;
+	opens aletheia.persistence.berkeleydb.entities.statement to je;
+	opens aletheia.persistence.berkeleydb.entities.local to je;
+	opens aletheia.persistence.berkeleydb.entities.peertopeer to je;
+	opens aletheia.persistence.berkeleydb.entities.misc to je;
+	opens aletheia.persistence.berkeleydb.proxies to je;
+	opens aletheia.persistence.berkeleydb.proxies.term to je;
+	opens aletheia.persistence.berkeleydb.proxies.parameteridentification to je;
+	opens aletheia.persistence.berkeleydb.proxies.security to je;
+	opens aletheia.persistence.berkeleydb.proxies.identifier to je;
+	opens aletheia.persistence.berkeleydb.proxies.net to je;
+	opens aletheia.persistence.berkeleydb.proxies.peertopeer.deferredmessagecontent to je;
+	
+	
 }
