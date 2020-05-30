09e86174dc5b2caefa5fe1f46490e56ee1b729a2
Quim Testar <quimtestar@gmail.com>
2020-05-30 18:52:34 +0200

    Warning fix.

--- Aletheia.GUI/src/aletheia/gui/cli/command/GlobalCommandFactory.java
+++ Aletheia.GUI/src/aletheia/gui/cli/command/GlobalCommandFactory.java
@@ -25,7 +25,6 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Paths;
