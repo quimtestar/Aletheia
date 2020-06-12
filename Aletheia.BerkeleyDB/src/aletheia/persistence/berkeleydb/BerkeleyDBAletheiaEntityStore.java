c47d9de4f6629f56609f209f626d742539b47e85
Quim Testar <quimtestar@gmail.com>
2020-06-12 18:46:03 +0200

    Entity store version set to 27.

--- Aletheia.BerkeleyDB/src/aletheia/persistence/berkeleydb/BerkeleyDBAletheiaEntityStore.java
+++ Aletheia.BerkeleyDB/src/aletheia/persistence/berkeleydb/BerkeleyDBAletheiaEntityStore.java
@@ -107,8 +107,8 @@ public class BerkeleyDBAletheiaEntityStore extends BerkeleyDBAletheiaAbstractEnt
 {
 	private static final Logger logger = LoggerManager.instance.logger();
 
-	private static final int storeVersion = 26;
-	private static final int minimalStoreVersion = 25;
+	private static final int storeVersion = 27;
+	private static final int minimalStoreVersion = 27;
 
 	private static final Collection<Class<?>> registerClasses = Arrays.<Class<?>> asList(
 	// @formatter:off
